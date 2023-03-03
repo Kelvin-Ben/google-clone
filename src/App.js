487ffe19d982dc4088134fc93833094d46ed13e7
Kelvin-Ben <kevoben14@gmail.com>
2023-03-04 02:18:55 +0300

    Create a few components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Footer from './components/Footer';
+import Navabr from './components/Navabr';
+import Routes from './components/Routes';
 
 const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false) 
+
   return (
-    <div>App</div>
+    // ternary operator
+    <div className={darkTheme ? 'dark' : ''}> 
+      <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+        <Navabr darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+        <Routes />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Routes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Routes = () => {
+  return (
+    <div>Routes</div>
+  )
+}
+
+export default Routes
